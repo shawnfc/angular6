1d6ccda21ede2420453cf64fd047c3b778011d5f
Shawn Conrad <shawnconrad@Shawns-MBP.attlocal.net>
2018-08-02 13:01:17 -0400

    components cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeesComponent } from './employees/employees.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule
